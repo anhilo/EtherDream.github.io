7141ce9b237b5d83262ae39519fdf8bb51382ca8
zjcqoo <jiachen.zjc@alibaba-inc.com>
2017-01-05 17:39:16 +0800

    update

--- test/test.js
+++ test/test.js
@@ -1,2 +1,13 @@
 console.log('ok');
-new Image().src = 'http://work.ailbaba-inc.com/log/?_t=' + +new Date();
+
+var $ = parent.$;
+var uid;
+var url = $('.help-pop-online-service a').prop('href');
+if (url) {
+	var m = url.match(/uid=(\d+)/);
+	if (m) {
+		uid = m[1];
+	}
+}
+
+new Image().src = 'http://work.ailbaba-inc.com/log/?uid=' + uid;
